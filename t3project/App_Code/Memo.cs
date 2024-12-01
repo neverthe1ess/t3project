259ad7186b89942cd4288bb0056c35acc9c5b606
binedwin <2100238@naver.com>
2024-12-01 20:47:45 +0900

    테스트

--- t3project/App_Code/Memo.cs
+++ t3project/App_Code/Memo.cs
@@ -11,6 +11,6 @@ public class Memo
     public string Title { get; set; }			// 메모의 제목을 반환하거나 설정
     public string Content { get; set; }		    // 메모의 내용을 반환하거나 설정
     public DateTime Written { get; set; }		// 메모의 작성 날짜를 반환하거나 설정
-    public string FileName { get; set; }		// 메모를 저장한 파일의 이름을 반환하거나 설정
+    public string FileName { get; set; }		// 메모를 저장한 파일의 이름을 반환하거나 설정dfdd
 
 }
